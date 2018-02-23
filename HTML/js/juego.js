cc146158ba51a1e8dcdc82b55ab13c0c3e41c173
rafade9 <rafade9@gmail.com>
2018-02-23 11:11:30 -0600

    Sesión 4

--- HTML/js/juego.js
+++ HTML/js/juego.js
@@ -1,1 +1,18 @@
-alert("carga JS");
+var vida = 100;
+var ataque = 10;
+var pocion = 20;
+
+function muestraVida(){
+  console.log("Nivel de vida del Héroe")
+  console.log(vida);
+}
+
+function ataqueEnemigo(){
+  vida -= ataque;
+  muestraVida();
+}
+
+function bebePocion(){
+  vida += pocion;
+  muestraVida();
+}
